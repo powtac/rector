1f6077d88f6bd70ddcbad8b68eef6ea57957dde3
dobryy <igordobritskiy@gmail.com>
2020-09-08 19:49:28 +0200

    [DX] Configurable rule must have configure code sample, and vice versa

---
+++ utils/phpstan-extensions/tests/Rule/ConfigurableRectorRule/Fixture/SkipClassNamesWithoutRectorWord.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PHPStanExtensions\Tests\Rule\ConfigurableRectorRule\Fixture;
+
+use Rector\Core\Contract\Rector\ConfigurableRectorInterface;
+use Rector\Core\RectorDefinition\CodeSample;
+use Rector\Core\RectorDefinition\ConfiguredCodeSample;
+use Rector\Core\RectorDefinition\RectorDefinition;
+use Rector\Transform\ValueObject\StaticCallToFuncCall;
+
+final class SkipClassNamesWithoutRectorWord
+{
+
+}
